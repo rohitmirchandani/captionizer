1784fa8a1ecc6dbf4d027aced9017507490250c3
rohitmirchandani <rohitmirchandani365@gmail.com>
2022-07-18 00:40:44 +0530

    initial commit

--- Frontend/src/helper.js
+++
@@ -1,3 +0,0 @@
-export const url = 'https://quotable.io/search/quotes?query=';
-export const urlRandomOne = 'https://quotable.io/random';
-export const urlRandomMany = 'https://quotable.io/quotes?page=3';
